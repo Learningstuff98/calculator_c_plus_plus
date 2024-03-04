6fd4754cabb3b987720ebfa74eb2db9452dbffa3
Andy Strube <andystrube64@gmail.com>
2024-03-04 14:52:37 -0800

    Basic skelton is set up

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include  <iostream>
 
+std::string get_desired_operation() {
+  std::cout << "Please enter one of the following options:" << std::endl;
+  std::cout << "add or exit." << std::endl;
+  std::string desired_operation {""};
+  getline(std::cin, desired_operation);
+  return desired_operation;
+};
+
 int main() {
-  std::cout << "Hello World from the calculator app." << std::endl;
+  std::string desired_operation {get_desired_operation()};
+  while(desired_operation != "exit") {
+    if(desired_operation == "add") {
+      std::cout << "ADD OPERATIONS GO HERE." << std::endl;
+      desired_operation = get_desired_operation();
+    } else {
+        std::cout << "ERROR! ERROR! INVALID SELECTION!" << std::endl;
+        desired_operation = get_desired_operation();
+    }
+  };
+  std::cout << "Program shutting down. Goodbye.";
 };
