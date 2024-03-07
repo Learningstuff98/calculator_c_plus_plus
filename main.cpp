6def00f3ac3e8193bc4ab78d0ccae135005fec22
Andy Strube <andystrube64@gmail.com>
2024-03-06 16:53:05 -0800

    Small refactor

--- main.cpp
+++ main.cpp
@@ -8,33 +8,44 @@ std::string get_desired_operation() {
   return desired_operation;
 };
 
-bool is_valid_integer(std::string const &int_input) {
-  if(int_input[0] == '0') {
+bool is_valid_integer(std::string const &input) {
+  if(input[0] == '0') {
     return false;
   }
-  for(auto const &int_char: int_input) {
-    if(int_char != '0' && int_char != '1' && int_char != '2' && int_char != '3' && int_char != '4' && int_char != '5' && int_char != '6' && int_char != '7' && int_char != '8' && int_char != '9') {
+  for(auto const &input_char: input) {
+    if(
+      input_char != '0' &&
+      input_char != '1' &&
+      input_char != '2' &&
+      input_char != '3' &&
+      input_char != '4' &&
+      input_char != '5' &&
+      input_char != '6' &&
+      input_char != '7' &&
+      input_char != '8' &&
+      input_char != '9'
+    ) {
       return false;
     }
   }
   return true;
 };
 
-std::string get_int(std::string message) {
+std::string get_input(std::string message) {
   std::cout << message << std::endl;
-  std::string int_input {""};
-  getline(std::cin, int_input);
-  if(is_valid_integer(int_input)) {
-    return int_input;
+  std::string input {""};
+  getline(std::cin, input);
+  if(is_valid_integer(input)) {
+    return input;
   } else {
-    return "ERROR!";
+    return "error";
   }
 };
 
 void add_integers() {
-  std::string int_1 {get_int("Enter the first number:")};
-  std::string int_2 {get_int("Enter the second number:")};
-  if(int_1 != "ERROR!" && int_2 != "ERROR!") {
+  std::string int_1 {get_input("Enter the first number:")};
+  std::string int_2 {get_input("Enter the second number:")};
+  if(int_1 != "error" && int_2 != "error") {
     std::cout << "The sum is: " << (std::stoi(int_1) + std::stoi(int_2)) << std::endl;
   } else {
     std::cout << "ERROR! ERROR! INVLID INPUT!" << std::endl;
